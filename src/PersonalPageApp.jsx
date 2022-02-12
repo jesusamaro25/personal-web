3edb7b54a774f10ef9eaf88155b96aa396b7b55f
Jesus Amaro <jesusamaro1995@gmail.com>
2022-02-11 20:18:23 -0400

    learning to use tailwind

---
+++ src/PersonalPageApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const PersonalPageApp = () => {
+  return (
+    <div className='main-background'>
+    </div>
+  )
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PersonalPageApp } from './PersonalPageApp';
+import './styles.css';
+import 'flowbite';
+
+ReactDOM.render(
+    <PersonalPageApp />,
+    document.getElementById('root')
+)
