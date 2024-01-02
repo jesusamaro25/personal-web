3c5df814beecac4aaaf5c7c20be7e91243123158
Jesus Amaro <jesusamaro1995@gmail.com>
2024-01-02 13:49:28 -0400

    init of updates

--- src/resume/Experience.jsx
+++ src/resume/Experience.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { aprilaugustConstant, binauralDescriptionConstant, binauralTitleConstant, ccoDescriptionConstant, ccoTitleConstant, drDescriptionConstant, drTitleConstant, innpactiaDescriptionConstant, innpactiaTitleConstant, julyConstant, marnaDescriptionConstant, marnaTitleConstant, octoberConstant, septemberaugustConstant, twothousandandeighteenConstant, twothousandandnineteenConstant, uclaDescriptionConstant, uclaTitleConstant } from '../words.constants'
+import { msbDescriptionConstant, msbTitleConstant, currentConstant, aprilaugustConstant, binauralDescriptionConstant, binauralTitleConstant, ccoDescriptionConstant, ccoTitleConstant, drDescriptionConstant, drTitleConstant, innpactiaDescriptionConstant, innpactiaTitleConstant, julyConstant, marnaDescriptionConstant, marnaTitleConstant, octoberConstant, septemberaugustConstant, twothousandandeighteenConstant, twothousandandnineteenConstant, uclaDescriptionConstant, uclaTitleConstant } from '../words.constants'
 
 export const Experience = () => {
   return (
@@ -37,11 +37,17 @@ export const Experience = () => {
         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{drTitleConstant}</h3>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">{drDescriptionConstant}</p>
       </li>
-      <li className="ml-4">
+      <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-gray-400 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{julyConstant}</time>
         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{innpactiaTitleConstant}</h3>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">{innpactiaDescriptionConstant}</p>
+      </li>
+      <li className="ml-4">
+        <div className="absolute w-3 h-3 bg-gray-400 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{currentConstant}</time>
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{msbTitleConstant}</h3>
+        <p className="text-base font-normal text-gray-500 dark:text-gray-400">{msbDescriptionConstant}</p>
       </li>
     </ol>
 
